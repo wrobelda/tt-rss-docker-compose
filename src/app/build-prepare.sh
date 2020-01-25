4e5e529eb6d51cf98d1d225c8f0141c7c20a631b
Andrew Dolgov <noreply@fakecake.org>
2020-01-25 16:20:00 +0300

    don't clone entire git history into container image

--- src/app/build-prepare.sh
+++ src/app/build-prepare.sh
@@ -4,7 +4,7 @@ DST_DIR=/src/tt-rss
 
 mkdir -p $DST_DIR
 
-git clone https://git.tt-rss.org/fox/tt-rss.git $DST_DIR
-git clone https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
+git clone --branch master --depth 1 https://git.tt-rss.org/fox/tt-rss.git $DST_DIR
+git clone --branch master --depth 1 https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
 
 mkdir -p /var/www
