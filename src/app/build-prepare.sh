6552ffd9dd62bb5d2f1aaad0880384697e56ba02
Andrew Dolgov <noreply@fakecake.org>
2020-01-25 15:33:04 +0300

    remove useless code from app build script

--- src/app/build-prepare.sh
+++ src/app/build-prepare.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 
 DST_DIR=/src/tt-rss
-SRC_REPO=https://git.tt-rss.org/fox/tt-rss.git
 
-if [ ! -d $DST_DIR ]; then
-	mkdir -p $DST_DIR
-	git clone $SRC_REPO $DST_DIR
-else
-	cd $DST_DIR && \
-		git config core.filemode false && \
-		git pull origin master
-fi
+mkdir -p $DST_DIR
 
-if [ ! -d $DST_DIR/plugins.local/nginx_xaccel ]; then
-	git clone https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
-else
-	cd $DST_DIR/plugins.local/nginx_xaccel && \
-		git config core.filemode false && \
-	  	git pull origin master
-fi
+git clone https://git.tt-rss.org/fox/tt-rss.git $DST_DIR
+git clone https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
 
 mkdir -p /var/www
-
