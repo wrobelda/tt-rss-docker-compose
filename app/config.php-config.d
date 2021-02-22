99a7753d92043921ca171ab11b98226731ccaeb3
Andrew Dolgov <noreply@fakecake.org>
2021-02-23 00:05:49 +0300

    config.php cleanup

--- app/config.php-config.d
+++
@@ -1,7 +0,0 @@
-	$snippets = glob(getenv("SCRIPT_ROOT")."/config.d/*.php");
-
-	foreach ($snippets as $snippet) {
-		require_once $snippet;
-	}
-
-
--- app/prepare-environment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-grep define config.php-dist | sed -e "s/[ \t]*define('\([A-Z_]\+\)', ['\"]\?\([^'\")]\+\).*/ENV TTRSS_\1=\"\2\"/" \
-  -e 's/"false"/""/'
