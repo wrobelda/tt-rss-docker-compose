15efbab11247a7bb80866e337e1fac7633a14769
Andrew Dolgov <noreply@fakecake.org>
2021-02-23 00:06:07 +0300

    config.php cleanup

--- src/app/prepare-config.php
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sed -e "s/define('\([A-Z_]\+\)', [^)]\+/define('\1', getenv('TTRSS_\1')/" \
-	< config.php-dist > config.docker.php
-
-cat config.php-config.d config.php-xaccel >> config.docker.php
