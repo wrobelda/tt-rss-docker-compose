4a7b2aed7050bf3a8c041eddc371eaecfece7356
Andrew Dolgov <noreply@fakecake.org>
2022-03-23 12:23:03 +0300

    * make origin repo configurable when building image  * halt build process if initial git clone fails  * log resulting commit id used when building

--- src/app/build-prepare.sh
+++ src/app/build-prepare.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
 DST_DIR=/src/tt-rss
 
 mkdir -p $DST_DIR
 
-git clone --branch master --depth 1 https://git.tt-rss.org/fox/tt-rss.git $DST_DIR
-git clone --branch master --depth 1 https://git.tt-rss.org/fox/ttrss-nginx-xaccel.git $DST_DIR/plugins.local/nginx_xaccel
+echo cloning $ORIGIN_REPO_MAIN to $DST_DIR...
+git clone --branch master --depth 1 $ORIGIN_REPO_MAIN $DST_DIR
+echo built for: $(git --git-dir=$DST_DIR/.git --no-pager log --pretty='%H' -n1 HEAD)
+
+echo cloning $ORIGIN_REPO_XACCEL to $DST_DIR/plugins.local/nginx_xaccel...
+git clone --branch master --depth 1 $ORIGIN_REPO_XACCEL $DST_DIR/plugins.local/nginx_xaccel
+echo built for: $(git --git-dir=$DST_DIR/plugins.local/nginx_xaccel/.git --no-pager log --pretty='%H' -n1 HEAD)
 
 mkdir -p /var/www
